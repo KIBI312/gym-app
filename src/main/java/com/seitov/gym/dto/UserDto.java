9b7ebb864caed4519aada5ed7ec150fe3d84fee7
KIBI312 <isken00.kg@gmail.com>
2023-10-04 15:05:00 +0600

    Adds AllArgsConstructor and NoArgsConstructors annotations to DTOs

--- src/main/java/com/seitov/gym/dto/UserDto.java
+++ src/main/java/com/seitov/gym/dto/UserDto.java
@@ -1,12 +1,16 @@
 package com.seitov.gym.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
     @NotBlank
--- src/main/java/com/seitov/gym/dto/UsernamePasswordDto.java
+++ src/main/java/com/seitov/gym/dto/UsernamePasswordDto.java
@@ -1,8 +1,12 @@
 package com.seitov.gym.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsernamePasswordDto {
 
     private String username;
