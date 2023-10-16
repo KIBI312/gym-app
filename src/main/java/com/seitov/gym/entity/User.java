3c7bb4fcf79529e56fff71af5194fae8bce8972c
KIBI312 <isken00.kg@gmail.com>
2023-10-16 15:03:11 +0600

    Adds UserDetails interface implementation to User entity

--- src/main/java/com/seitov/gym/entity/User.java
+++ src/main/java/com/seitov/gym/entity/User.java
@@ -2,11 +2,17 @@ package com.seitov.gym.entity;
 
 import javax.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Data
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_generator")
@@ -22,5 +28,30 @@ public class User {
     private String password;
     @Column(name = "is_active")
     private Boolean isActive;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.<GrantedAuthority>singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
 
 }
